7cca3f450844b326aa7068c60b14f73c23c7b334
DEMONDAV <david.moscoso@udla.edu.ec>
2026-05-12 11:15:19 -0500

    primera opción de impresion de arreglos con punteros

--- puntarray.c
+++ puntarray.c
@@ -4,11 +4,17 @@
 int main()
 {
     int a [5] = {2,4,6,8,10};
+    int *ptr;
+    ptr = &a[0];
 
    
    for (int i = 0; i < 5; i++)
    {
-    printf("%d ",a [i]);
+    
+    printf("arrelog [%d] = %d ", i, a [i]);
+    ptr = &a[i];
+    printf("%d\n",*ptr);
+
    }
    
 
