3beede3047a5cc6b6e78856e4b8f1282436ebab2
DEMONDAV <david.moscoso@udla.edu.ec>
2026-05-12 10:46:43 -0500

    definicion basica de arreglo

---
+++ puntarray.c
@@ -0,0 +1,16 @@
+#include<stdio.h>  //segundo concepto paso por referencia 
+
+
+int main()
+{
+    int a [5] = {2,4,6,8,10};
+
+   
+   for (int i = 0; i < 5; i++)
+   {
+    printf("%d ",a [i]);
+   }
+   
+
+}
+    
