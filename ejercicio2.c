aac291d2b4ee31ad722e97c9a596b9bf10d58a28
DEMONDAV <david.moscoso@udla.edu.ec>
2026-05-12 11:42:03 -0500

    ejercicio 2 impresion de array char basica

---
+++ ejercicio2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>  //segundo concepto paso por referencia 
+
+
+int main()
+{
+   int vocales [5] = {'a','e','i','o','u'};
+
+   
+   for (int i = 0; i < 5; i++)
+   {
+    
+    printf("%c",vocales[i]);
+
+   }
+}
